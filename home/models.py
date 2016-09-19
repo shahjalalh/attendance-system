2070aa803a67557e62b006504b84b9ab29963c99
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2016-09-19 17:35:22 +0600

    login page added

--- home/models.py
+++ home/models.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
